c9e46d1b7d97e6899121cae9ac8f42e9ade05497
EverettE-codes <everettqedwards@gmail.com>
2026-09-13 19:19:54 -0400

    Update paths for config files in installdebian.sh

--- debian/installdebian.sh
+++ debian/installdebian.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p ~/.config/fastfetch
-cp ~/debian/config.jsonc ~/.config/fastfetch/config.jsonc
-cp ~/debian/starship.toml ~/.config/starship.toml
+cp ~/fastfetch-starship-rices/debian/config.jsonc ~/.config/fastfetch/config.jsonc
+cp ~/fastfetch-starship-rices/debian/starship.toml ~/.config/starship.toml
 
